d82c5b7e1ca021520ea1ded944dbf20534ed1950
Lars Bieber <lbieber1@smail.uni-koeln.de>
2024-12-11 12:07:24 +0100

    Map Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
 function App() {
   return (
-    <div className='flex items-center justify-center'>
-     <p>TEST</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p>TEST</p>
+      <div className="w-full h-3/4">
+        <MapContainer 
+          center={[50.863, 10.339]} // Coordinates for the map center
+          zoom={6} 
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty popup! <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
