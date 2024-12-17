b29f798ba23ea0e3237b57a6b9b0c6e8d2a9655f
Lars Bieber <lbieber1@smail.uni-koeln.de>
2024-12-17 16:19:45 +0100

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 import Map from './components/map';
