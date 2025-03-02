b0b3cd4c7b6b9a1d23d70b8c39d08cae1f2b1926
Lars Bieber <lbieber1@smail.uni-koeln.de>
2025-03-02 13:00:24 +0100

    small change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,9 +13,9 @@ export default function Navbar(){
                         <button className="hover:bg-blue-400 px-4 py-2 rounded">Map</button>
                     </Link>
                     <Link to="/test">
-                    <button className="hover:bg-blue-400 px-4 py-2 rounded">Lorem Ipsum</button>
+                    <button className="hover:bg-blue-400 px-4 py-2 rounded">Der Hexenhammer</button>
                     </Link>
-                    <button className="hover:bg-blue-400 px-4 py-2 rounded">Button 3</button>
+                    <button className="hover:bg-blue-400 px-4 py-2 rounded">Quellen</button>
                     <button className="hover:bg-blue-400 px-4 py-2 rounded">Button 4</button>
                 </div>
             </div>
