9fda5a6cc23e0fb52b856e65f89c04ac055c2f44
Lars Bieber <lbieber1@smail.uni-koeln.de>
2025-03-05 12:29:37 +0100

    Sentences/Means of Execution are now displayed in German

--- src/components/trials_places.js
+++ src/components/trials_places.js
@@ -23,13 +23,15 @@ export default function Trials_places() {
             const groupedMarkers = results.data.reduce((acc, row) => {
               const city = row.place?.toLowerCase().trim();
               const date = row.date?.trim();
+              const sentence = row.sentence?.trim();
               const coordinates = places[city];
 
               if (coordinates && date) {
                 if (!acc[city]) {
-                  acc[city] = { city: capitalizeFirstLetter(city), coordinates, dates: [] };
+                  acc[city] = { city: capitalizeFirstLetter(city), coordinates, dates: [], sentences: [] };
                 }
                 acc[city].dates.push(date);
+                acc[city].sentences.push(sentence);
               }
               return acc;
             }, {});
@@ -56,12 +58,17 @@ export default function Trials_places() {
           stroke={false} 
         >
           <Popup>
-            <strong>Hexenprozess</strong><br />
+            <strong>Hexenprozess</strong><br/>
             <strong>Ort:</strong> {marker.city}
             <br />
             {marker.dates.map((date, i) => (
               <div key={i}>
-                <strong>Jahr:</strong> {date}
+                <strong>Jahr:</strong> {date}<br />
+                {marker.sentences[i] &&(
+                <>
+                <strong>Urteil/Hinrichtungsmethode:</strong> {marker.sentences[i]}
+                </>
+                )}
               </div>
             ))}
           </Popup>
