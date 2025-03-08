07cbbc02a2102d84b543a6e74b284ae6dab95a6e
Lars Bieber <lbieber1@smail.uni-koeln.de>
2025-03-08 15:00:37 +0100

    Changed Imperial Circles GIS

--- src/components/katholisch.js
+++ src/components/katholisch.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import * as L from "leaflet";
-import Kath1 from "../Reichskreise/Katholisch/12044.json"
-import Kath2 from "../Reichskreise/Katholisch/12048.json"
-import Kath3 from "../Reichskreise/Katholisch/12049.json"
-import Kath4 from "../Reichskreise/Katholisch/12050.json"
-import Kath5 from "../Reichskreise/Katholisch/12051.json"
-import Kath6 from "../Reichskreise/Katholisch/12052.json"
-import Kath7 from "../Reichskreise/Katholisch/12053.json"
-import Kath8 from "../Reichskreise/Katholisch/12054.json"
+import Kath1 from "../Reichskreise/Katholisch/11976.json"
+import Kath2 from "../Reichskreise/Katholisch/11977.json"
+import Kath3 from "../Reichskreise/Katholisch/11978.json"
+import Kath4 from "../Reichskreise/Katholisch/11982.json"
+import Kath5 from "../Reichskreise/Katholisch/11984.json"
+import Kath6 from "../Reichskreise/Katholisch/11985.json"
+import Kath7 from "../Reichskreise/Katholisch/11986.json"
 
 
 
@@ -66,14 +65,6 @@ export default function Katholisch() {
               },
         }),
         L.geoJSON(Kath7, {
-            style: {
-                color: "#7cc28f",
-                weight: 2,
-                opacity: 1,
-                fillOpacity: 0.5,
-              },
-        }),
-        L.geoJSON(Kath8, {
             style: {
                 color: "#7cc28f",
                 weight: 2,
--- src/components/protestantisch.js
+++ src/components/protestantisch.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import * as L from "leaflet";
-import Prot1 from "../Reichskreise/Protestantisch/12045.json"
-import Prot2 from "../Reichskreise/Protestantisch/12046.json"
-import Prot3 from "../Reichskreise/Protestantisch/12047.json"
+import Prot1 from "../Reichskreise/Protestantisch/11979.json"
+import Prot2 from "../Reichskreise/Protestantisch/11980.json"
+import Prot3 from "../Reichskreise/Protestantisch/11981.json"
+import Prot4 from "../Reichskreise/Protestantisch/11983.json"
+
+
 
 export default function Protestantisch() {
     const map = useMap();
@@ -33,7 +36,15 @@ export default function Protestantisch() {
                 opacity: 1,
                 fillOpacity: 0.5,
               },
-        })
+        }),
+        L.geoJSON(Prot4, {
+          style: {
+              color: "#ff7800",
+              weight: 2,
+              opacity: 1,
+              fillOpacity: 0.5,
+            },
+      })
       ];
   
       geoJsonLayers.forEach(layer => layer.addTo(map));
